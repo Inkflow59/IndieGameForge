db5301a9ee98db17d0cdbc0a49bad3d92aeb4842
Inkflow <tomcucherosset@hotmail.fr>
2024-03-27 16:47:51 +0100

    Correction du fichier SQL

--- db/IndieGameForge.sql
+++ db/IndieGameForge.sql
@@ -1,10 +1,13 @@
+CREATE DATABASE IF NOT EXISTS IndieGameForge;
+USE IndieGameForge;
+
 DROP TABLE IF EXISTS Developpeur ;
 CREATE TABLE Developpeur (idDev INT AUTO_INCREMENT NOT NULL,
 nomDev VARCHAR(50),
 prenomDev VARCHAR(50),
 paysDev VARCHAR(50),
 anneeDebutDev YEAR,
-dateNaissance_Developpeur DATE,
+dateNaissance DATE,
 dateInscription DATE,
 username VARCHAR(50),
 password VARCHAR(50),
@@ -26,8 +29,7 @@ DROP TABLE IF EXISTS Note ;
 CREATE TABLE Note (idDev INT AUTO_INCREMENT NOT NULL,
 idJeu INT NOT NULL,
 noteJeu FLOAT,
-PRIMARY KEY (idDev,
- idJeu)) ENGINE=InnoDB;
+PRIMARY KEY (idDev,idJeu)) ENGINE=InnoDB;
 
 ALTER TABLE Jeu ADD CONSTRAINT FK_Jeu_idDev FOREIGN KEY (idDev) REFERENCES Developpeur (idDev);
 
